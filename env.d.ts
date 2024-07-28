c113e2bd8ec78b87c3d636f5243adf4acfba787d
tryhafsops <hafsahill17@gmail.com>
2024-07-29 04:59:02 +0500

    env change

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,12 @@
-declare namespace NodeJS {
-    export interface ProcessEnv {
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
         NEXT_PUBLIC_SERVICE_ID : string;
         NEXT_PUBLIC_TEMPLATE_ID:string;
         NEXT_PUBLIC_PUBLIC_KEY:string;
         ENVRIONMENT: "development" | "production";
+      }
     }
   }
+  
+  export {};
