8c3cfe41ae980393add623fe96adc9b564635de9
tryhafsops <hafsahill17@gmail.com>
2024-07-29 05:03:02 +0500

    .env

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,10 @@
 declare global {
     namespace NodeJS {
       interface ProcessEnv {
-        NEXT_PUBLIC_SERVICE_ID : string;
-        NEXT_PUBLIC_TEMPLATE_ID:string;
-        NEXT_PUBLIC_PUBLIC_KEY:string;
-        ENVRIONMENT: "development" | "production";
+        NEXT_PUBLIC_SERVICE_ID : string ='service_x75e0gy';
+        NEXT_PUBLIC_TEMPLATE_ID:string = 'template_3n0i118';
+        NEXT_PUBLIC_PUBLIC_KEY:string= 'GUWDpr9xP5SO0z-5e';
+     
       }
     }
   }
