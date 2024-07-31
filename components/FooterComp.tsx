5e062700ade6c814f811303420e9661f930b3646
tryhafsops <hafsahill17@gmail.com>
2024-07-31 18:36:04 +0500

    footer change

--- components/FooterComp.tsx
+++ components/FooterComp.tsx
@@ -6,12 +6,12 @@ import {BsGithub, BsLinkedin} from 'react-icons/bs'
 const FooterComp = () => {
   return (
     <Footer container className='border border-t-8 border-gray-900 rounded-none '>
-        <div className='w-full max-w-7xl mx-auto '>
+        <div className='w-full max-w-7xl mx-auto mb-20'>
             
             <Footer.Divider/>
             <div className='w-full flex flex-row items-center justify-between'>
                 <Footer.Copyright href='#' by='Hafsa Dev' year={new Date().getFullYear()} className='text-md sm:text-lg'/>
-                <div className='flex gap-6 mt-2 justify-center'>
+                <div className='flex gap-6 mt-2 justify-center '>
                     <Footer.Icon href='https://www.github.com/hafsascript' icon={BsGithub}/>
                     <Footer.Icon href='#' icon={BsLinkedin}/>
                 </div>
